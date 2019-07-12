42af9b05dec50fc9411ec05a2a1044c8e54e35c1
huangshuai <huangshuai@qiyi.com>
2019-07-12 17:56:00 +0800

    add service

---
+++ data_analysis/src/main/java/com/example/data_analysis/controller/DfsJobController.java
@@ -0,0 +1,13 @@
+package com.example.data_analysis.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DfsJobController {
+
+    @GetMapping(value="/start/job")
+    public void startJob(){
+
+    }
+}
---
+++ data_analysis/src/main/java/com/example/data_analysis/service/hadoop/Reduce.java
@@ -0,0 +1,4 @@
+package com.example.data_analysis.service.hadoop;
+
+public class Reduce {
+}
