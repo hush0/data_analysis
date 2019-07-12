ea0fb0be3d44be94e7fa50694af725c80618a7e6
huangshuai <huangshuai@qiyi.com>
2019-07-12 17:36:25 +0800

    add yml dataSource

--- data_analysis/src/main/java/com/example/data_analysis/controller/HelloController.java
+++ data_analysis/src/main/java/com/example/data_analysis/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.data_analysis.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-
+    @GetMapping(value ="/hello")
+    public String sayHello(){
+        return "hello";
+    }
 }
